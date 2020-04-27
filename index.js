f32f224189ff75564a0f78a0f3cbe5acc9fa6c9b
austinckohler <austinckohler@gmail.com>
2020-04-26 18:33:42 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// let element = document.createElement('div') 
+// element.innerHTML = "<h1> just a test</h1><p>i am writing a paragraph now</p><h2>this is just a test for a h2 tag </h2>"
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
